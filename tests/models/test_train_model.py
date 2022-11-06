0828a34bd84ff4d11f427f0837e0d877f5370533
Grigory Kulagin <grigorykulagin4276@gmail.com>
2022-11-06 16:01:16 +0300

    Add tests

--- tests/models/test_train_model.py
+++ tests/models/test_train_model.py
@@ -1,9 +1,22 @@
+from typing import Union
+
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
 from src.models import train_model
 from src.entities import TrainingParams
 
 
-def test_train_model(features: pd.DataFrame, target: pd.Series, train_params: TrainingParams):
-    model = train_model(features, target, train_params)
+def test_train_rf_model(transformed_dataframe: Union[pd.DataFrame, pd.Series], training_params_rf: TrainingParams):
+    features, target = transformed_dataframe
+    model = train_model(features, target, training_params_rf)
     assert isinstance(model, RandomForestClassifier)
+
+
+def test_train_log_reg_model(transformed_dataframe: Union[pd.DataFrame, pd.Series], training_params_log_reg: TrainingParams):
+    features, target = transformed_dataframe
+    model = train_model(features, target, training_params_log_reg)
+    assert isinstance(model, LogisticRegression)
+
+
